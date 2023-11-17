f8549073908302c6a66abcba5db14124ead39248
tth <xyz@foo.com>
2023-11-17 10:24:02 +0800

    Create Cloud Scheduler service account to invoke Cloud Run

--- scheduler/main.tf
+++ scheduler/main.tf
@@ -8,6 +8,11 @@ resource "google_project_service" "run_api" {
   disable_on_destroy = false
 }
 
+resource "google_project_service" "iam_api" {
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_cloud_run_v2_service" "default" {
   name     = var.service_name
   location = var.region
@@ -26,4 +31,21 @@ resource "google_cloud_run_v2_service" "default" {
 
 output "service_url" {
   value = google_cloud_run_v2_service.default.uri
+}
+
+resource "google_service_account" "default" {
+  account_id   = "scheduler-sa"
+  description  = "Cloud Scheduler service account; used to trigger scheduled Cloud Run jobs."
+  display_name = "scheduler-sa"
+
+  depends_on = [
+    google_project_service.iam_api
+  ]
+}
+
+resource "google_cloud_run_service_iam_member" "default" {
+  location = google_cloud_run_v2_service.default.location
+  service  = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.default.email}"
 }
