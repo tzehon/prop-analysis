f17aed4c518e8d9c292e6882db3ae0a38eccb723
tth <xyz@foo.com>
2023-11-19 00:20:16 +0800

    Invoke cron

--- scheduler/main.tf
+++ scheduler/main.tf
@@ -13,6 +13,11 @@ resource "google_project_service" "iam_api" {
   disable_on_destroy = false
 }
 
+resource "google_project_service" "scheduler_api" {
+  service            = "cloudscheduler.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_cloud_run_v2_service" "default" {
   name     = var.service_name
   location = var.region
@@ -48,4 +53,31 @@ resource "google_cloud_run_service_iam_member" "default" {
   service  = google_cloud_run_v2_service.default.name
   role     = "roles/run.invoker"
   member   = "serviceAccount:${google_service_account.default.email}"
+}
+
+resource "google_cloud_scheduler_job" "default" {
+  name             = "scheduled-cloud-run-job"
+  region           = var.region
+  description      = "Invoke a Cloud Run container on a schedule."
+  schedule         = "0 6 * * SUN"
+  time_zone        = "Asia/Singapore"
+  attempt_deadline = "320s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "GET"
+    uri         = google_cloud_run_v2_service.default.uri
+
+    oidc_token {
+      service_account_email = google_service_account.default.email
+    }
+  }
+
+  # Use an explicit depends_on clause to wait until API is enabled
+  depends_on = [
+    google_project_service.scheduler_api
+  ]
 }
